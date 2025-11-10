d1dcfaf5603fb168bfc1891573ef0b42b60de9d2
LAPTOP-E6CQABTS\Tran Hoai Duy <hoaiduy688@gmail.com>
2025-11-10 17:42:42 +0700

    fix send mail

--- app/Models/AccountModel.php
+++ app/Models/AccountModel.php
@@ -1,7 +1,10 @@
 <?php
+
 namespace App\Models;
 
 use App\Core\Model;
+use PDOException;
+
 
 class AccountModel extends Model
 {
@@ -17,4 +20,29 @@ class AccountModel extends Model
         'role',
         'status'
     ];
+    public function insert(array $data)
+    {
+        try {
+            $sql = "INSERT INTO {$this->table} 
+                    (email, password, full_name, created_at, updated_at, role, status) 
+                    VALUES (:email, :password, :full_name, :created_at, :updated_at, :role, :status)";
+
+            $stmt = $this->db->prepare($sql);
+
+            $stmt->execute([
+                'email'      => $data['email'],
+                'password'   => $data['password'],
+                'full_name'  => $data['full_name'],
+                'created_at' => $data['created_at'],
+                'updated_at' => $data['updated_at'],
+                'role'       => $data['role'] ?? 'user',
+                'status'     => $data['status'] ?? 1
+            ]);
+
+            return (int)$this->db->lastInsertId(); // ID vừa tạo
+        } catch (PDOException $e) {
+            error_log("Account insert error: " . $e->getMessage());
+            return false;
+        }
+    }
 }
